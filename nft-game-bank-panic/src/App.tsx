af940721fd70c4e7839e3d332edadcb64f5a994a
David <davidtorroija@gmail.com>
2022-05-31 16:21:56 -0500

    fixing bug in canvas

--- nft-game-bank-panic/src/App.tsx
+++ nft-game-bank-panic/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
 import phaserGame from './PhaserGame'
 import HelloWorldScene from './scenes/HelloWorldScene'
@@ -17,26 +15,8 @@ const handleClick = () => {
 //   applyMiddleware(thunkMiddleware, createLogger())
 // );
 
-function App() {
-  return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Just a vanilla create-react-app overlaying a Phaser canvas :)</p>
-        <a
-          className="App-link"
-          href="https://github.com/kevinshen56714/create-react-phaser3-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Source
-        </a>
-        <button className="App-button" onClick={handleClick}>
-          Or click me
-        </button>
-      </header> */}
-    </div>
-  )
+function App(): JSX.Element {
+  return (<></>)
 }
 
 export default App
