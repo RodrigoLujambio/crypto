1069ce9a45c298387d040e24598b0063e2098338
David <davidtorroija@gmail.com>
2022-06-02 17:17:40 -0500

    redux toolkit it's alive and working!

--- nft-game-bank-panic/src/App.tsx
+++ nft-game-bank-panic/src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css'
 import Phaser from "phaser";
 import gameConfig from './PhaserGame'
-
+import { useDispatch } from 'react-redux';
 // const handleClick = () => {
 //   const scene = phaserGame.scene.keys.helloworld as HelloWorldScene
 //   scene.createEmitter()
 // }
 
+import { increment, decrement, incrementByAmount } from "./store/counter-slice";
+
 let game: Phaser.Game | null = null;
 
 function App(): JSX.Element {
+  const dispatch = useDispatch();
+
   if (!game) {
+    dispatch(increment())
     game = new Phaser.Game(gameConfig);
   }
   return (<></>)
